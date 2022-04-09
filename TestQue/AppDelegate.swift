ab30b48526ee7b8f113c584064d20b5410028bd9
mikhail <lokal1122@mail.ru>
2022-04-09 20:38:59 +0300

    New

--- TestQue/AppDelegate.swift
+++ TestQue/AppDelegate.swift
@@ -10,11 +10,11 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+//    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-                          window?.rootViewController = ViewController()
-                          window?.makeKeyAndVisible()
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//                          window?.rootViewController = ViewController()
+//                          window?.makeKeyAndVisible()
         
         Thread.sleep(forTimeInterval: 1.1)
         // Override point for customization after application launch.
